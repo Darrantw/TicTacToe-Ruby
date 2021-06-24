29f026a731730aad28e620026b60fd5632e5c389
DarranTWood <darrantwood@gmail.com>
2021-06-24 21:48:21 +0700

    Made program OOP

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,243 +1,288 @@
 #TicTacToe.rb
-
-=begin
-module MOVES
-  end
-=end
+#Last thing to do: restrict input so that choice ONLY accepts 1-9.#
 class HumanPlayer
-  attr_writer :accessor
+  attr_accessor :name, :piece, :move
   
   def initialize
-      puts "What's your name?"
-      @name = gets
-      puts @name
+    puts "New player created!"
     end
 
-  def name
-    self.name = @name
+  def name_setter
+    puts "Player, enter your name, please:"
+    @name = gets.chomp
   end
   
+  def name
+    @name
   end
 
-  class ComputerPlayer
-
-      def initialize
-       
-      end
+  def move(game)
+    puts "#{self.name}, type the number(as a digit) you'd like to make a #{self.piece}."
+    choice = gets.chomp.to_i
     
+    if (((choice.size < 1) || (choice.size > 1)) && ((choice > 9) && (choice < 1)))
+      puts "Please enter a digit 1-9"
+      move(game) 
     end
+  
+       
+    case choice
+    when 1
+      if game.chosen_include(1)
+        puts "Try again."
+         self.move(game)
+      else 
+         game.one = self.piece
+         game.chosen.push(1)
+      end
+    when 2
+      if game.chosen_include(2)
+          puts "Try again."
+          self.move(game)
+      else 
+          game.two = self.piece
+          game.chosen.push(2)
+      end
+    when 3
+      if game.chosen_include(3)
+        puts "Try again."
+         self.move(game)
+      else 
+        game.three = self.piece
+         game.chosen.push(3)
+      end
+    when 4
+      if game.chosen_include(4)
+          puts "Try again."
+          self.move(game)
+      else 
+          game.four = self.piece
+          game.chosen.push(4)
+      end
+    when 5
+      if game.chosen_include(5)
+          puts "Try again."
+          self.move(game)
+      else 
+            game.five = self.piece
+             game.chosen.push(5)
+      end
+    when 6
+      if game.chosen_include(6)
+          puts "Try again."
+          self.move(game)
+      else 
+          game.six = self.piece
+          game.chosen.push(6)
+      end
+    when 7
+      if game.chosen_include(7)
+          puts "Try again."
+          self.move(game)
+      else 
+          game.seven = self.piece
+          game.chosen.push(7)
+      end
+    when 8
+      if game.chosen_include(8)
+          puts "Try again."
+          self.move(game)
+      else 
+          game.eight = self.piece
+          game.chosen.push(8)
+      end
+    when 9
+      if game.chosen_include(9)
+          puts "Try again."
+          self.move(game)
+      else 
+          game.nine = self.piece
+          game.chosen.push(9)
+      end
+    end
+  end
+
+  private
+  def piece
+     @piece
+  end
+
+end 
 
   
 class TicTacToe
-# This line doesn't work yet  attr-writer @one
+  attr_accessor :one, :two, :three, :four, :five, :six, :seven, :eight, :nine
   def initialize
+    @count = 0
     @one = 1
     @two = 2
-    @three = 3
-    @four = 4
+    @three = 3 
+    @four = 4 
     @five = 5
-    @six = 6 
+    @six = 6
     @seven = 7
-    @eight = 8
-    @nine = 9
-    @winner = false
-    @turns = 0
-    @draw = false
-    @p1_choice = 0
-    @humans = 1
-    @computer
+    @eight = 8 
+    @nine = 9 
+    @chosen = Array.new   
+    end
+
+    def count
+      @count
+    end
+
+    def one
+      @one
+    end
+
+    def two
+      @two
+    end
+
+    def three
+      @three
+    end
+
+    def four
+      @four
+    end
+
+    def five
+      @five
+    end
+
+    def six
+      @six
+    end
+
+    def seven
+      @seven
+    end
+
+    def eight
+      @eight
+    end
+
+    def nine
+      @nine
+    end
+
     
-    @player1_piece
-    
-    @player2_piece
-    @computer_piece
-  end
 
-  def how_many_humans
-    puts "1 or 2 human players?"
-    @humans = gets.chomp
-    if @humans == '1'
-      player1 = HumanPlayer.new
-
-      @computer = ComputerPlayer.new
-      puts "You will play the computer"
-    elsif @humans == '2'
-      player1 = HumanPlayer.new
-      player2 = HumanPlayer.new
-    else
-      puts "Sorry, choose 1 or 2 only."
-      TicTacToe.how_many_humans
-    end
-  end
-
-
-  def assign_pieces
-    if @humans == '1'
-      @player1_piece = rand(2)
-      if @player1_piece ? (@player1_piece = 'X' && @computer_piece = 'O') : (@player1_piece = 'O' && @computer_piece = 'X')
-                puts "#{@player1}, you are #{@player1_piece} and the computer is #{@computer_piece}."
-      end
-    elsif @humans === '2'
-      @player1_piece = rand(2)
-      if @player1_piece ? ((@player1_piece = 'X') && (@player2_piece = 'O')) : ((@player1_piece = 'O') && (@player2_piece = 'X'))
-        puts "#{@player1}, you are #{@player1_piece} and #{@player2}, you are #{@player2_piece}."
-      end
-    end
-  end
-
-  def squares
-    {@one => 1, 
-    @two => 2,
-    @three => 3, 
-    @four =>4, 
-    @five =>5, 
-    @six =>6, 
-    @seven => 7, 
-    @eight => 8, 
-    @nine => 9 }
-  end
-
-  def occupancy_check
-    if (@p1_choice === 1) && (@one === 'X' || @one === 'O')
-     return true
-    end
-    if (@p1_choice === 2) && (@two === 'X' || @two === 'O')
-      return true
-     end
-     if (@p1_choice === 3) && (@three === 'X' || @three === 'O')
-      return true
-     end
-     if (@p1_choice === 4) && (@four === 'X' || @four === 'O')
-      return true
-     end
-     if (@p1_choice === 5) && (@five === 'X' || @five === 'O')
-      return true
-     end
-     if (@p1_choice === 6) && (@six === 'X' || @six === 'O')
-      return true
-     end
-     if (@p1_choice === 7) && (@seven === 'X' || @seven === 'O')
-      return true
-     end
-     if (@p1_choice === 8) && (@eight === 'X' || @eight === 'O')
-      return true
-     end
-     if (@p1_choice === 9) && (@nine === 'X' || @nine === 'O')
-      return true
-     end
-    end
-
-    def xoccupied
-      puts "This square is already taken, please choose an available square instead."
-      xmove
-      
-    end
-    
-    def ooccupied
-      puts "This square is already taken, please choose an available square instead."
-      omove
-      
-    end
-  
-
-  def result
-    winner = false
-    if (@one == 'X'  && @two == 'X'  && @three == 'X' ) || (@four == 'X' && @five == 'X' && @six === 'X') || (@seven === 'X' && @eight === 'X' && @nine === 'X') || (@seven === 'X' && @four === 'X' && @one === 'X') || (@eight === 'X' && @five === 'X' && @two === 'X') || (@nine === 'X' && @six === 'X' && @three === 'X') || (@seven === 'X' && @five === 'X' && @three === 'X') || (@one === 'X' && @five === 'X' && @nine === 'X')
-      puts "X is the winner!"
-    @winner = true
-  elsif (@one == 'O' && @two == 'O' && @three == 'O' ) || (@four == 'O' && @five == 'O' && @six === 'O') || (@seven == 'O' && @eight == 'O' && @nine === 'O') || (@seven == 'O' && @four == 'O' && @one === 'O') || (@eight == 'O' && @five == 'O' && @two === 'O') || (@nine == 'O' && @six == 'O' && @three === 'O') || (@seven == 'O' && @five == 'O' && @three === 'O') || (@one == 'O' && @five == 'O' && @nine === 'O')
-      puts "O is the winner!"
-      @winner = true
-  elsif (@turns == 9 && @winner == false)
-      @draw = true
-      puts "Draw!"
-  end
-  end
 
   def display
     puts "#{@seven} | #{@eight} | #{@nine}"
     puts "#{@four} | #{@five} | #{@six}"
     puts "#{@one} | #{@two} | #{@three}"
   end
-# below are not working properly. Typing anything results in first conditional being met.
 
-  def xmove
-    puts "Type the number(as a digit) you'd like to make a cross"
-    @p1_choice = gets.to_i
-    if (@p1_choice < 1 || @p1_choice > 9)
-      puts "Please enter a digit 1-9"
-      xmove
+  def chosen
+    @chosen
+  end
+
+  def chosen_include(num)
+    @chosen.include?(num)
+  end
+
+  def assign_pieces
+      @player1_piece = rand(2)
+      @player1_piece == 1 ? (@player1_piece = 'X') && (@player2_piece = 'O') : (@player1_piece = 'O') && (@player2_piece = 'X')
+        puts "#{@player1name}, you are #{@player1_piece} and #{@player2name}, you are #{@player2_piece}."
     end
-      if @p1_choice === 1
-        occupancy_check ?  xoccupied : @one = 'X' 
-      elsif @p1_choice === 2
-        occupancy_check ? xoccupied : @two = 'X'
-      elsif @p1_choice === 3
-        occupancy_check ? xoccupied : @three = 'X'
-      elsif @p1_choice == 4
-        occupancy_check ? xoccupied : @four = 'X'
-      elsif @p1_choice === 5
-        occupancy_check ? xoccupied : @five= 'X'
-      elsif @p1_choice === 6
-        occupancy_check ? xoccupied : @six = 'X'
-      elsif @p1_choice == 7
-        occupancy_check ? xoccupied : @seven = 'X'
-      elsif @p1_choice === 8
-        occupancy_check ? xoccupied : @eight = 'X'
-      elsif @p1_choice === 9
-        occupancy_check ? xoccupied : @nine = 'X'
+
+  def player1name(player1)
+    @player1name = player1.name
+  end
+
+  def player2name(player2)
+    @player2name = player2.name
+  end
+
+  def player1_piece
+    @player1_piece
+  end
+
+  def player2_piece
+    @player2_piece
+  end
+  
+  def play(player1, player2)
+    starter = rand(2)
+    starter == 1 ? player1_starts(player1, player2) : player2_starts(player1, player2)
+  end
+
+  private 
+  def lines
+    lines = [@one, @two, @three], [@four, @five, @six], [@seven, @eight, @nine], 
+    [@one, @four, @seven], [@two, @five, @eight], [@three, @six, @nine], 
+    [@one, @five, @nine], [@three, @five, @seven]
+  end
+
+  def win_checker
+    for i in lines
+      if i == [player1_piece, player1_piece, player1_piece]
+        puts "#{@player1name} is the winner."
+        @win_checker = 1
+      elsif i == [player2_piece, player2_piece, player2_piece]
+        puts "#{@player2name} is the winner."
+        @win_checker = 1
       end
-  end
-
-  def omove
-    puts "Type the number you'd like to make a naught"
-    @p1_choice = gets.to_i
-    if (@p1_choice < 1 || @p1_choice > 9)
-      puts "Please enter a digit 1-9"
-      omove
-    end
-
-    if @p1_choice === 1
-      occupancy_check ? ooccupied : @one = 'O'
-    elsif @p1_choice === 2
-      occupancy_check ? ooccupied : @two = 'O'
-    elsif @p1_choice === 3
-      occupancy_check ? ooccupied : @three = 'O'   
-    elsif @p1_choice == 4
-      occupancy_check ? ooccupied : @four = 'O'
-    elsif @p1_choice === 5
-      occupancy_check ? ooccupied : @five= 'O'
-    elsif @p1_choice === 6
-      occupancy_check ? ooccupied : @six = 'O'
-    elsif @p1_choice == 7
-      occupancy_check ? ooccupied : @seven = 'O'
-    elsif @p1_choice === 8
-      occupancy_check ? ooccupied : @eight = 'O'
-    elsif @p1_choice === 9
-      occupancy_check ? ooccupied : @nine = 'O'
     end
   end
 
-=begin
- def run
-#    player1 = HumanPlayer.new
- 
+  def post_move(player)
+    display
+    win_checker
+    @count += 1
+    if @count == 9 
+      puts "It's a draw."
+    end
+    
+  end
+
+  def player1_starts(player1, player2)
+    puts "#{@player1name} has been randomly chosen to go first."
+    display
+    
+
+    loop do
+      player1.move(self)
+      post_move(player1)
+      break if @win_checker == 1
+      break if @count == 9
+      player2.move(self)
+      post_move(player2)
+      break if @win_checker == 1
+      break if @count == 9
     end
   end
-=end
 
-end
+  def player2_starts(player1, player2)
+    puts "#{@player2name} has been randomly chosen to go first."
+    display
+    count = 0
 
+    loop do
+      player2.move(self)
+      post_move(player2)
+      break if @win_checker == 1
+      break if @count == 9
+      player1.move(self)
+      post_move(player1)
+      break if @win_checker == 1
+      break if @count == 9
+    end
+  end
 
- 
+  end
 
-
-
-
+player1 = HumanPlayer.new
+player1.name_setter
+player2 = HumanPlayer.new
+player2.name_setter
 game = TicTacToe.new
-game.how_many_humans
+game.player1name(player1)
+game.player2name(player2)
 game.assign_pieces
-
-
-#game.play
-#game.run
+player1.piece = game.player1_piece
+player2.piece = game.player2_piece
+game.play(player1, player2)
